0e8319e6765a5e5c4a80e5ab4a9865b0848fad8e
Rudresh Mishra <rudreshmishra721@gmail.com>
2025-10-22 23:51:58 +0530

    updated the solution of Twosum problem

--- array/TwoSum.js
+++ array/TwoSum.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 function twoSum(nums, target) {
-    let newMap = new Map();
+    let Newmap = new Map();
 
  for(let i = 0; i < nums.length; i++){
     let num = nums[i]
     let complement = target - num
-    if(newMap.has(complement)){
-        return [newMap.get(complement), i] // return[newMap.get(2) --> 0 , 1 (current index)] == [0,1]
+    if(Newmap.has(complement)){
+        return [Newmap.get(complement), i] // return[newMap.get(2) --> 0 , 1 (current index)] == [0,1]
     }
-    newMap.set(num, i) // storing num as key and i as value means 2 --> 0
+    Newmap.set(num, i) // storing num as key and i as value means 2 --> 0
     
 
     }
