2ec7e888f87615465a5dade080e3b93679ccdad6
Rudresh Mishra <rudreshmishra721@gmail.com>
2025-10-26 23:09:46 +0530

    added palimdrome number problem

---
+++ math/palindrome-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+    var isPalindrome = function(x) {
+    if(x<0){
+        return false
+    }
+    let str = x.toString();
+    let rev = str.split('').reverse().join('')
+     return str === rev
+
+};
