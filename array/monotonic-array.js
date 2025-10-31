1e1246d11d3cade3038f1e81bc736a9d23b7f5dc
Rudresh Mishra <rudreshmishra721@gmail.com>
2025-11-01 00:01:35 +0530

    added monotonic array  problem solution

---
+++ array/monotonic-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < nums[i + 1]) {
+      decreasing = false;
+    } else if (nums[i] > nums[i + 1]) {
+      increasing = false;
+    }
+  }
+
+  return increasing || decreasing;
+};
