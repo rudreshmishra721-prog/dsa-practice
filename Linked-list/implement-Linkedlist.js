fe10801a370d4effbb1f53a318e90a8ea9e36958
Rudresh Mishra <rudreshmishra721@gmail.com>
2025-11-09 23:57:45 +0530

    adding insert methon in implementation of linked-list

--- Linked-list/implement-Linkedlist.js
+++ Linked-list/implement-Linkedlist.js
@@ -17,7 +17,36 @@ class Linkedlist {
     this.length++;
     return this;
     }
+     insert(index , value){
+        if(index === 0){
+            return this.prepend(value);
+        }
+        if(index >= this.lenght){
+            return this.append(value);
+        }
+        const newNode = {
+            value: value,
+            next : null,
+        };
+        const leader = this.traverseToIndex(index - 1)
+        const holdingppointer = leader.next;
+        leader.next = newNode
+        newNode.next  = holdingppointer;
+        this.length++
+        return this.printiList()
+    }
+     traverseToIndex(index){
+            let counter = 0;
+            let currentNode = this.head;
+            while(counter !== index){
+                currentNode = currentNode.next;
+                counter++;
+            }
+            return currentNode
+        } 
 }  
 const mylinkedlist = new Linkedlist(10)
 mylinkedlist.append(20)
-console.log(mylinkedlist)
+mylinkedlist.insert(0, 99);
+mylinkedlist.insert(3, 94);
+console.log(mylinkedlist.printiList())
