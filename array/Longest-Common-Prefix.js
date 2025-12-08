05e132018267ce329f4ad40c8601f3ef7222f949
Rudresh Mishra <rudreshmishra721@gmail.com>
2025-12-08 21:45:41 +0530

    adding question no 14 Longest Common Prefix

---
+++ array/Longest-Common-Prefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+
+    let first = strs[0];
+
+    for (let i = 0; i < first.length; i++) {
+        let ch = first[i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== ch) {
+                return first.slice(0, i);
+            }
+        }
+    }
+
+    return first;
+    
+};
