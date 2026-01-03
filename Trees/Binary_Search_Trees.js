51645e1c95be4cc4d7d87b3727d80a1a2c72bd02
Rudresh Mishra <rudreshmishra721@gmail.com>
2026-01-03 23:10:19 +0530

    Implemented Insert Operation in Binary Search Tree

--- Trees/Binary_Search_Trees.js
+++ Trees/Binary_Search_Trees.js
@@ -11,8 +11,30 @@ class BinarySearchTree {
     this.root = null;
   }
   insert(value) {
-    
-  }
+    const newNode = new Node(value);
+    if(this.root === null){
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true){
+        if(value < currentNode.value){
+        //Left
+        if(!currentNode.left){
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left
+      } else {
+        //Right
+        if(!currentNode.right){
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  } 
+ }
   lookup(value) {
     
   }
@@ -30,9 +52,9 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-tree.remove(170);
+// tree.remove(170);
 JSON.stringify(traverse(tree.root));
-console.log(tree.lookup(20));
+// console.log(tree.lookup(20));
 //     9
 //  4     20
 //1  6  15  170
@@ -41,5 +63,5 @@ function traverse(node) {
   const tree = { value: node.value };
   tree.left = node.left === null ? null : traverse(node.left);
   tree.right = node.right === null ? null : traverse(node.right);
-  return tree;
-}
+  return console.log (tree);
+} 
